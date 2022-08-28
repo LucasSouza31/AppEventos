0efadcf3051a0b53cd782ad8c5c81cba660ca45d
lucassouza31 <lucaseasy10@gmail.com>
2022-08-27 22:14:53 -0300

    Adicionando listagem

--- src/main/java/com/Eventos/AppEventos/Controllers/EventoController.java
+++ src/main/java/com/Eventos/AppEventos/Controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.Eventos.AppEventos.Models.Evento;
 import com.Eventos.AppEventos.Repository.EventoRepository;
@@ -30,7 +31,14 @@ public String form(Evento evento) {
     return "redirect:/cadastrarEvento";
 }
 
+@RequestMapping("/eventos")
+public ModelAndView listaeventos() {
+    ModelAndView mv= new ModelAndView("index");
+    Iterable<Evento> eventos= event.findAll();
+    mv.addObject("eventos",eventos);
 
+    return mv;
+}
 
 
 
